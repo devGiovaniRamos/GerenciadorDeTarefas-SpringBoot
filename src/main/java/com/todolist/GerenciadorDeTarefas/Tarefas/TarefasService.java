1fd181ff294726b65e07eece2f4d74b02cb0414d
Giovani Ramos <pgiovani970@gmail.com>
2026-01-09 21:32:04 -0300

    add TarefasService

--- src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasService.java
+++ src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasService.java
@@ -1,4 +1,14 @@
 package com.todolist.GerenciadorDeTarefas.Tarefas;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class TarefasService {
+
+    private TarefaRepository tarefaRepository;
+
+    public void cadastrarTarefa(TarefasModel cadastrar) {
+        tarefaRepository.save(cadastrar);
+    }
 }
