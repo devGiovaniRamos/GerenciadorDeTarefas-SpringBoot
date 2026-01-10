dbd80d4a073fb04cc5e3e0c63b38b858ef4e8f16
Giovani Ramos <pgiovani970@gmail.com>
2026-01-10 00:36:06 -0300

    add migrations flyway

--- src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasService.java
+++ src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasService.java
@@ -1,14 +1,22 @@
 package com.todolist.GerenciadorDeTarefas.Tarefas;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public class TarefasService {
 
+    @Autowired
     private TarefaRepository tarefaRepository;
 
     public void cadastrarTarefa(TarefasModel cadastrar) {
         tarefaRepository.save(cadastrar);
+    }
+
+    public List<TarefasModel> listarTarefas() {
+        return tarefaRepository.findAll();
     }
 }
