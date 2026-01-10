52b6e47100e65ca45297786e21760553aefb6a8c
Giovani Ramos <pgiovani970@gmail.com>
2026-01-10 11:45:29 -0300

    add endpoints e criando logica no Service

--- src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasController.java
+++ src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasController.java
@@ -1,25 +1,37 @@
 package com.todolist.GerenciadorDeTarefas.Tarefas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefa")
 public class TarefasController {
 
+    @Autowired
+    private TarefasService tarefasService;
+
     //ROTAS...
 
     //Cadastrar tarefa
-    @GetMapping("/cadastrar")
-    public String helloword() {
-        return "HelloWord!";
+    @PostMapping("/cadastrar")
+    public TarefasModel cadastrar(@RequestBody TarefasModel tarefasModel) {
+        return tarefasService.cadastrarTarefa(tarefasModel);
     }
 
     //Ver tarefas
+    @GetMapping("/listar")
+    public List<TarefasModel> listar() {
+        return tarefasService.listarTarefas();
+    }
 
     //Atualizar tarefa
 
     //Deletar tarefa
+    @DeleteMapping("/deletar/{id}")
+    public void deletarTarefa(@PathVariable Long id) {
+        tarefasService.deletarTarefa(id);
+    }
 
 }
--- src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasService.java
+++ src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasService.java
@@ -2,6 +2,9 @@ package com.todolist.GerenciadorDeTarefas.Tarefas;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,11 +15,19 @@ public class TarefasService {
     @Autowired
     private TarefaRepository tarefaRepository;
 
-    public void cadastrarTarefa(TarefasModel cadastrar) {
-        tarefaRepository.save(cadastrar);
+    public TarefasModel cadastrarTarefa(TarefasModel cadastrar) {
+        return tarefaRepository.save(cadastrar);
     }
 
     public List<TarefasModel> listarTarefas() {
         return tarefaRepository.findAll();
     }
+
+    //Atualizar tarefa
+
+
+    public void deletarTarefa(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+
 }
