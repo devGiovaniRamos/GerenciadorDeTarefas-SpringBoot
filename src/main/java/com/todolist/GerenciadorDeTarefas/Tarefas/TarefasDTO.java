b71d6730f238471689659eb2942112b9185ad689
Giovani Ramos <pgiovani970@gmail.com>
2026-01-16 23:11:09 -0300

    add: Mapper e DTO

--- src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasDTO.java
+++ src/main/java/com/todolist/GerenciadorDeTarefas/Tarefas/TarefasDTO.java
@@ -1,4 +1,30 @@
 package com.todolist.GerenciadorDeTarefas.Tarefas;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class TarefasDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private LocalDate data;
+
+    private boolean concluida;
+
+    public TarefasDTO(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.data = LocalDate.now();
+        this.concluida = false;
+    }
 }
